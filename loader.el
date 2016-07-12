5544077ae004e9edc774c26f55903a0bbe0ef59c
Joakim Jalap <joakim.jalap@gmail.com>
2016-07-12 09:57:27 +0200

    Custom stuff
    I should really set custom-file or w/e it's called.

--- loader.el
+++ loader.el
@@ -2,3 +2,17 @@
 (require 'org)
 (require 'ob-tangle)
 (org-babel-load-file "~/.emacsconf/conf.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (irony debbugs soap-client ztree xkcd xcscope w3m undo-tree sx srefactor spotify solarized-theme smex rtags rich-minority pyenv-mode py-autopep8 projectile pinentry pdf-tools pandoc-mode origami org-caldav notmuch multiple-cursors multi-term move-text mentor magit-svn magit-gh-pulls linum-off lice imenu-anywhere ignoramus idomenu helm-ag google-translate gnuplot gitignore-mode gitconfig-mode ggtags function-args fold-dwim flx-ido expand-region eww-lnum emms elfeed dsvn dired+ diff-hl conkeror-minor-mode company-math company-irony company-c-headers company-auctex company-anaconda comment-dwim-2 browse-url-dwim bbdb avy-zap ace-window ace-link))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
